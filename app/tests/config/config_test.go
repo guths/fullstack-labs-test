be9f70b2971c28b55c36288806d50e977df9c092
Filipe Oliveira <filipe@fullstacklabs.co>
2022-10-23 08:28:32 -0300

    Add unit tests for config file load

--- app/tests/config/config_test.go
+++ app/tests/config/config_test.go
@@ -1,9 +1,29 @@
 package tests_config
 
-import "testing"
+import (
+	"battle-of-monsters/app/config"
+	tests_utils "battle-of-monsters/app/tests/utils"
+	"testing"
+)
 
 func TestLoad(t *testing.T){
-	if true != true {
-		t.Errorf("Error load configs")
+
+	tests_utils.LoadEnv()
+
+	port := config.ENV.Port
+	driver := config.ENV.DBDriver
+	dbName := config.ENV.DBName
+
+	if port != "8080" {
+		t.Errorf("Port expected as 8080 but got %v", port)
 	}
+
+	if driver != "sqlite" {
+		t.Errorf("Driver expected as sqlite but got %v", driver)
+	}
+
+	if dbName != "db/db.test.sqlite" {
+		t.Errorf("Database name expected as db/db.test.sqlite but got %v", dbName)
+	}
+
 }
---
+++ app/tests/utils/envutils.go
@@ -0,0 +1,30 @@
+package tests_utils
+
+import (
+	"battle-of-monsters/app/config"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+// this should match the project root folder name.
+const rootFolder = "assessment-cc-go-sr-01"
+
+// LoadEnv load the env files relative to the root folder from any nested test directory.
+func LoadEnv() {
+	re := regexp.MustCompile(fmt.Sprintf("^(.*%s)", rootFolder))
+	cwd, _ := os.Getwd()
+	rootPath := re.Find([]byte(cwd))
+	errMsg := "update the `testutils.rootFolder` variable to match the name of the project root folder"
+
+	if rootPath == nil {
+		panic(errMsg)
+	}
+
+	if err := os.Chdir(string(rootPath)); err != nil {
+		panic(errMsg)
+	}
+
+	os.Setenv("GO_ENVIRONMENT", "TEST")
+	config.Load()
+}
